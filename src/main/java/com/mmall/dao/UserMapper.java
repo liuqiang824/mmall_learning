bc6a885a97bb617d03929298aa1f1ec85fb25c19
liuqiang <531803037@qq.com>
2018-03-18 15:42:11 +0800

    注册登录

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,21 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    /**
+     * 查询用户是否存在
+     * @param username
+     * @return
+     */
+    int checkUsername(String username);
+
+    int checkEmail(String email);
+
+    /**
+     * 查询登录用户和密码
+     * @param username
+     * @param password
+     * @return
+     */
+    User selectLogin(@Param("username") String username,@Param("password")String password);
 }
---
+++ src/main/java/com/mmall/sevice/impl/UserServiceImpl.java
@@ -0,0 +1,78 @@
+package com.mmall.sevice.impl;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.sevice.IUserService;
+import com.mmall.util.MD5Util;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("iUserService")
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int resultCount = userMapper.checkUsername(username);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+        //todo密码登录MD5
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username,md5Password);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("密码错误");
+        }
+
+        //为什么这块密码置为空
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.createBySuccess("登录成功",user);
+    }
+
+    public ServerResponse<String> register(User user){
+        int resultCount = userMapper.checkUsername(user.getUsername());
+        if(resultCount > 0){
+            return ServerResponse.createByErrorMessage("用户名已存在");
+        }
+        resultCount = userMapper.checkEmail(user.getEmail());
+        if(resultCount > 0){
+            return ServerResponse.createByErrorMessage("email名已存在");
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+
+        resultCount = userMapper.insert(user);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+            return ServerResponse.createBySuccessMessage("注册成功");
+    }
+
+    public ServerResponse<String> checkValid(String str, String type){
+        if(StringUtils.isBlank(type)){
+            //开始校验
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkUsername(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("用户名已存在");
+                }
+            }
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(type);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("email名已存在");
+                }
+            }
+        }else {
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+        return ServerResponse.createBySuccessMessage("校验成功");
+    }
+
+}
